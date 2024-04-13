80dd3a2629c727a3d4f01bbd154b99199390f9a8
Keegan <keegantournay@gmail.com>
2024-04-13 09:36:38 -0400

    New commands

---
+++ commands/info/changelog.py
@@ -0,0 +1,32 @@
+from discord import Embed
+from discord.ext import commands
+
+info = {
+    "name": "changelog",
+    "aliases": ["updates", "changes", "cl"],
+    "description": "Displays the most recent changes for the bot",
+}
+
+
+class Changelog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=info["aliases"])
+    async def changelog(self, ctx):
+        with open("./changelog.txt", "r") as file:
+            change_list = "".join(file.readlines()).split("\n\n")
+
+        description = "\n\n".join([change for change in change_list[::-1][:5]])
+
+        embed = Embed(
+            title="TypeRacer Stats - Changelog",
+            description=description,
+            color=0,
+        )
+
+        await ctx.send(embed=embed)
+
+
+async def setup(bot):
+    await bot.add_cog(Changelog(bot))
---
+++ commands/owner/lockdown.py
@@ -0,0 +1,38 @@
+from discord import Embed
+from discord.ext import commands
+import colors
+from commands.checks import owner_check
+
+info = {
+    "name": "lockdown",
+    "aliases": ["ld"],
+    "description": "Terminates bot usage for everyone except the owner",
+}
+
+class Lockdown(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.lockdown = False
+
+    @commands.command(aliases=info["aliases"])
+    @commands.check(owner_check)
+    async def lockdown(self, ctx):
+        if not self.lockdown:
+            self.bot.add_check(owner_check)
+            await ctx.send(embed=Embed(
+                title=":rotating_light: Lockdown Initiated :rotating_light:",
+                description="Users will be unable to use commands during this time",
+                color=colors.error,
+            ))
+            self.lockdown = True
+        else:
+            self.bot.remove_check(owner_check)
+            await ctx.send(embed=Embed(
+                title="Lockdown Ended",
+                description="Users may now use commands again",
+                color=colors.success,
+            ))
+            self.lockdown = False
+
+async def setup(bot):
+    await bot.add_cog(Lockdown(bot))
