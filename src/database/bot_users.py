466d91e098cb0a123983490e0cf8ce3007c76d26
Keegan <keegantournay@gmail.com>
2024-08-06 23:09:42 -0400

    Fixed new user error

--- src/database/bot_users.py
+++ src/database/bot_users.py
@@ -24,7 +24,7 @@ def get_user(ctx):
     """, [user_id])
 
     if not user:
-        return None
+        return add_user(user_id)
 
     user = dict(user[0])
     user["colors"] = json.loads(user["colors"])
