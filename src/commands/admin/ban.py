b8beefa17518f896f11e20405b7a6d382a374795
Keegan <keegantournay@gmail.com>
2024-08-17 14:08:03 -0400

    Prevented people from banning themselves...

--- src/commands/admin/ban.py
+++ src/commands/admin/ban.py
@@ -2,6 +2,7 @@ from discord import Embed
 from discord.ext import commands
 
 from commands.checks import admin_check
+from config import bot_owner
 from database.banned import get_banned, ban, unban
 from database.bot_users import get_user
 from utils import errors, colors
@@ -34,19 +35,23 @@ class Ban(commands.Cog):
         except:
             return await ctx.send(embed=Embed(title="Unknown User", color=colors.error))
 
-        await run(ctx, user, user_id)
+        await run(ctx, user, str(user_id))
 
 
 async def run(ctx, user, user_id):
     banned = get_banned()
+    author_id = str(ctx.author.id)
 
     embed = Embed(color=user["colors"]["embed"])
 
-    if user_id == "155481579005804544":
-        ban(str(ctx.author.id))
-        embed = Embed(color=user["colors"]["embed"])
+    if user_id == author_id:
+        embed.title = "I Wouldn't Recommend That..."
+        embed.description = f"Why would you willingly take away the\njoy of using the bot from yourself?"
+
+    elif user_id == str(bot_owner):
+        ban(author_id)
         embed.title = "You Dare Even Try?"
-        embed.description = f"<@{ctx.author.id}> has just voided their bot privileges\nfor attempting to ban the owner"
+        embed.description = f"<@{author_id}> has just voided their bot privileges\nfor attempting to ban the owner"
 
     elif user_id in banned:
         unban(user_id)
