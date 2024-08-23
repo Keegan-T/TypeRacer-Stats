1ddb6805c73a4cc1190e45458f23ac138007bf26
Keegan <keegantournay@gmail.com>
2024-08-23 15:50:45 -0400

    Keegan message updating

--- src/commands/unlisted/keegan.py
+++ src/commands/unlisted/keegan.py
@@ -1,6 +1,9 @@
 from discord import Embed
 from discord.ext import commands
 
+from config import bot_owner, root_dir
+
+message_path = root_dir + "\\src\\data\\message.txt"
 command = {
     "name": "keegan",
     "aliases": ["keegant", "kegnat", "kt"],
@@ -13,17 +16,31 @@ class Keegan(commands.Cog):
         self.bot = bot
 
     @commands.command(aliases=command["aliases"])
-    async def keegan(self, ctx):
+    async def keegan(self, ctx, *args):
+        if args and ctx.author.id == bot_owner:
+            message = " ".join(args)
+            return await update_message(ctx, message)
+
         await run(ctx)
 
 
 async def run(ctx):
+    with open(message_path, "r", encoding="utf-8") as file:
+        message = "".join(file.readlines())
+
     embed = Embed(
-        description="Welcome To The Multiverse.",
+        description=message,
         color=0,
     )
 
     await ctx.send(embed=embed)
+
+
+async def update_message(ctx, message):
+    with open(message_path, "w", encoding="utf-8") as file:
+        file.writelines(message)
+
+    await run(ctx)
 
 
 async def setup(bot):
