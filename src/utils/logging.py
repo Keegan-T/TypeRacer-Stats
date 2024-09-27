8561e7afe379567f05a2c83e99336264ec5ba982
Keegan <keegantournay@gmail.com>
2024-09-27 16:54:35 -0400

    Improved logging to support long tracebacks

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,3 +1,4 @@
+import tempfile
 import time
 import traceback
 
@@ -40,21 +41,30 @@ def get_log_message(message):
     return f"{message_link} {mention}{linked_account}: `{content}`"
 
 
-def log(message):
+def log(message, file=None):
     if staging:
         return print(message)
+
+    if file:
+        return requests.post(webhook, data={"content": message}, files={"file": file})
 
     requests.post(webhook, json={"content": message})
 
 
-def log_error(log_message, error):
+def log_error(command_message, error):
     if staging:
         return traceback.print_exception(type(error), error, error.__traceback__)
 
+    log_message = f"<@{bot_owner}>\n{command_message}\n"
     error_traceback = traceback.format_exception(type(error), error, error.__traceback__)
+    traceback_string = "".join([line for line in error_traceback])
 
-    log(
-        f"<@{bot_owner}>\n"
-        f"{log_message}\n"
-        f"```ansi\n\u001B[2;31m{''.join([line for line in error_traceback])}\u001B[0m```"
-    )
+    if len(traceback_string) >= 1800:
+        with tempfile.NamedTemporaryFile(mode="w+", suffix=".txt", delete=True) as temp_file:
+            temp_file.write(traceback_string)
+            temp_file.flush()
+            temp_file.seek(0)
+            log(log_message, temp_file)
+
+    log_message += f"```ansi\n\u001B[2;31m{traceback_string}\u001B[0m```"
+    log(log_message)
