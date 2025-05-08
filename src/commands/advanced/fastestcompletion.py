a93a5e933bdf7708280ecbf3747640f5473aaa41
Keegan <keegantournay@gmail.com>
2025-05-08 01:35:18 -0400

    Added buttons to -fastestcompletion

--- src/commands/advanced/fastestcompletion.py
+++ src/commands/advanced/fastestcompletion.py
@@ -3,11 +3,12 @@ from discord.ext import commands
 
 import database.races as races
 import database.users as users
-from commands.advanced.races import add_stats
+from commands.advanced.races import get_stats_fields
 from commands.locks import big_lock
 from database import texts
 from database.bot_users import get_user
 from utils import errors, colors, strings, embeds
+from utils.embeds import Message, Page, is_embed
 
 categories = ["races", "points"]
 command = {
@@ -35,7 +36,7 @@ class FastestCompletion(commands.Cog):
         user = get_user(ctx)
 
         result = get_args(user, args, command)
-        if embeds.is_embed(result):
+        if is_embed(result):
             return await ctx.send(embed=result)
 
         username, number, category = result
@@ -57,20 +58,17 @@ async def run(ctx, user, username, number, category):
     if not stats:
         return await ctx.send(embed=errors.import_required(username, universe))
 
-    if stats["races"] > 100_000:
-        if big_lock.locked():
-            return await ctx.send(embed=errors.large_query_in_progress())
-        await big_lock.acquire()
-
     era_string = strings.get_era_string(user)
     if era_string:
         stats = await users.time_travel_stats(stats, user)
 
-    if (category == "races" and number > stats["races"]) or number > stats["points"]:
+    if (category == "races" and number > stats["races"]) or number > stats["points"] + stats["points_retroactive"]:
         return await ctx.send(embed=no_milestone(category, universe), content=era_string)
 
-    if category == "races":
-        number = round(number)
+    if stats["races"] > 100_000:
+        if big_lock.locked():
+            return await ctx.send(embed=errors.large_query_in_progress())
+        await big_lock.acquire()
 
     text_list = texts.get_texts()
     text_lengths = {text["id"]: len(text["quote"]) for text in text_list}
@@ -80,68 +78,89 @@ async def run(ctx, user, username, number, category):
         username, columns=columns, universe=universe, start_date=user["start_date"], end_date=user["end_date"]
     )
     race_list.sort(key=lambda x: x[7])
-    fastest = float("inf")
-    race_range = []
-    start_index = 0
-    difference = float("inf")
+
+    windows = []
+    race_count = len(race_list)
 
     if category == "races":
-        end_index = number
-        while end_index < len(race_list):
+        number = round(number)
+        for start_index in range(race_count - number + 1):
+            end_index = start_index + number
             start_race = race_list[start_index]
             start_time = start_race[7] - (text_lengths[start_race[0]] * 12) / start_race[2]
             end_race = race_list[end_index - 1]
-            difference = end_race[7] - start_time
-            if difference < fastest:
-                fastest = difference
-                race_range = [start_index, end_index]
-            start_index += 1
-            end_index += 1
-
-        if difference < fastest:
-            race_range = [start_index, end_index]
+            duration = end_race[7] - start_time
+            windows.append([start_index, end_index, duration])
 
     else:
+        start_index = 0
         total_points = 0
-        single = False
-        for end_index in range(0, len(race_list)):
-            points = race_list[end_index][4]
-            if points >= number:
-                single = True
-            total_points += points
-            if total_points >= number:
-                while total_points - race_list[start_index][4] >= number:
-                    total_points -= race_list[start_index][4]
-                    start_index += 1
+        for end_index in range(race_count):
+            total_points += race_list[end_index][4]
+            while total_points >= number:
                 start_race = race_list[start_index]
                 start_time = start_race[7] - (text_lengths[start_race[0]] * 12) / start_race[2]
-                difference = race_list[end_index][7] - start_time
-                if difference < fastest:
-                    fastest = difference
-                    race_range = [start_index, end_index + 1]
-            if single:
-                race_range = [end_index, end_index + 1]
+                end_time = race_list[end_index][7]
+                duration = end_time - start_time
+                windows.append((start_index, end_index + 1, duration))
+                total_points -= race_list[start_index][4]
+                start_index += 1
+
+    windows.sort(key=lambda x: x[2])
+
+    top_windows = []
+    used_ranges = []
+    for start, end, duration in windows:
+        overlap = False
+        for s, e in used_ranges:
+            if not (end <= s or start >= e):
+                overlap = True
+                break
+        if not overlap:
+            top_windows.append((start, end, duration))
+            used_ranges.append((start, end))
+            if len(top_windows) == 10:
                 break
 
-    if not race_range:
-        completion_races = race_list
-        fastest = completion_races[-1][7] - completion_races[0][7]
-    else:
-        completion_races = race_list[race_range[0]:race_range[1]]
-    start_time = completion_races[0][7]
-    end_time = completion_races[-1][7]
-
-    embed = Embed(
-        title=f"Fastest Completion ({number:,} {category.title()})",
-        description=f"{strings.format_duration_short(fastest, False)}",
-        color=user["colors"]["embed"],
+    fastest = top_windows[0]
+    fastest_time = fastest[2]
+    race_range = race_list[fastest[0]:fastest[1]]
+    start_time = race_range[0]["timestamp"]
+    end_time = race_range[-1]["timestamp"]
+    fields, footer = get_stats_fields(
+        username, race_range, start_time, end_time, universe="play"
     )
 
-    embeds.add_profile(embed, stats, universe)
-    add_stats(embed, username, completion_races, start_time, end_time, universe=universe)
-    embeds.add_universe(embed, universe)
+    description = ""
+    for i in range(len(top_windows)):
+        window = top_windows[i]
+        duration = window[2]
+        start_number = race_list[window[0]]["number"]
+        end_number = race_list[window[1] - 1]["number"]
+        description += (
+            f"{i + 1}. **{strings.format_duration_short(duration, False)}** "
+            f"(Races {start_number:,} - {end_number:,})\n"
+        )
 
-    await ctx.send(embed=embed, content=era_string)
+    pages = [
+        Page(
+            f"Fastest Completion ({number:,} {category.title()})",
+            f"{strings.format_duration_short(fastest_time, False)}",
+            fields, footer, "Fastest",
+        ),
+        Page(
+            f"Top 10 Fastest ({number:,} {category.title()})",
+            description, button_name="Top 10",
+        )
+    ]
+
+    message = Message(
+        ctx, user, pages,
+        profile=stats,
+        universe=universe,
+    )
+
+    await message.send()
 
     if big_lock.locked():
         big_lock.release()
