70c432268ddbe4f2e75a20b6c1306a3973a6c39c
Keegan <keegantournay@gmail.com>
2025-05-30 20:39:56 -0400

    Added accuracy to -racehistory

--- src/commands/advanced/racehistory.py
+++ src/commands/advanced/racehistory.py
@@ -15,7 +15,7 @@ from utils.embeds import Message, get_pages, is_embed
 from utils.stats import calculate_seconds
 
 periods = ["races", "day", "week", "month", "year"]
-sorts = ["date", "points", "races", "time", "wpm"]
+sorts = ["date", "points", "races", "time", "wpm", "accuracy"]
 command = {
     "name": "racehistory",
     "aliases": ["rh"],
@@ -87,14 +87,16 @@ async def run(ctx, user, username, time_period, sort):
     else:
         def formatter(period):
             return (
-                f"**{period[1]}**\n{period[3]:,.0f} pts / {period[2]:,} races - "
-                f"{period[4]:,.2f} WPM - {strings.format_duration_short(period[5])}\n\n"
+                f"**{period[1]}**\n"
+                f"{period[2]:,} Races / {period[3]:,.0f} Points / "
+                f"{strings.format_duration_short(period[5])}\n"
+                f"{period[4]:,.2f} WPM ({period[6]:.2%} Accuracy)\n\n"
             )
 
         sort_title = {"wpm": "WPM"}.get(sort, sort.title())
         title = f"Race History - {time_period.title()}s (By {sort_title})"
         history = await get_history(username, time_period, sort, universe, user["start_date"], user["end_date"])
-        pages = get_pages(history, formatter, page_count=10, per_page=10)
+        pages = get_pages(history, formatter, page_count=20, per_page=5)
 
     message = Message(
         ctx, user, pages,
@@ -107,8 +109,8 @@ async def run(ctx, user, username, time_period, sort):
 
 
 async def get_history(username, category, sort, universe, start_date, end_date):
-    sort_key = {"points": 3, "races": 2, "time": 5, "wpm": 4}.get(sort, 0)
-    columns = ["text_id", "wpm", "points", "timestamp"]
+    sort_key = {"points": 3, "races": 2, "time": 5, "wpm": 4, "accuracy": 6}.get(sort, 0)
+    columns = ["text_id", "wpm", "points", "timestamp", "accuracy"]
     race_list = await races.get_races(
         username, columns=columns, universe=universe, start_date=start_date, end_date=end_date
     )
@@ -117,7 +119,7 @@ async def get_history(username, category, sort, universe, start_date, end_date):
     history = []
 
     for race in race_list:
-        text_id, wpm, points, timestamp = race
+        text_id, wpm, points, timestamp, accuracy = race
         seconds = calculate_seconds(text_list[text_id]["quote"], wpm)
 
         if len(history) == 0 or timestamp > history[-1][1]:
@@ -136,7 +138,7 @@ async def get_history(username, category, sort, universe, start_date, end_date):
 
             history.append([
                 start.timestamp(), end,
-                1, points, wpm, seconds,
+                1, points, wpm, seconds, accuracy
             ])
 
         else:
@@ -144,6 +146,7 @@ async def get_history(username, category, sort, universe, start_date, end_date):
             history[-1][3] += points
             history[-1][4] += wpm
             history[-1][5] += seconds
+            history[-1][6] += accuracy
 
     for stats in history:
         start = datetime.fromtimestamp(stats[0], tz=timezone.utc)
@@ -157,6 +160,7 @@ async def get_history(username, category, sort, universe, start_date, end_date):
         else:
             stats[1] = start.strftime("%Y")
         stats[4] = stats[4] / stats[2]
+        stats[6] = stats[6] / stats[2]
 
     history = sorted(history, key=lambda x: x[sort_key], reverse=True)
 
