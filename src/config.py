7ab2b6ed22987c61ccf2fff90338353f0887e0bd
Keegan <keegantournay@gmail.com>
2025-09-13 06:24:09 -0400

    Added API credentials

--- src/config.py
+++ src/config.py
@@ -51,3 +51,4 @@ typeracer_stats_channel_id = 746460695670816798
 
 donate_link = "https://www.paypal.com/donate/?business=X9JW4MC3CLNAE&no_recurring=0&currency_code=USD&item_name=TypeRacer+Stats"
 web_server = f"http://{os.getenv('ip')}"
+api_credentials = os.getenv("api_credentials").split(",")[1:]
